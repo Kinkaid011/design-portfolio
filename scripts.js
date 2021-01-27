696301ee5786faf51f0e8c061af31a3f239b723c
Kinkaid011 <k.nolen46@gmail.com>
2021-01-27 10:44:34 -0500

    edited height with image and tweaked logo

--- scripts.js
+++ scripts.js
@@ -14,13 +14,13 @@ gsap.timeline()
 
 
 
-gsap.from("#c1", {scrollTrigger: { trigger:"#c1", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: 1, ease: "back"});
-gsap.from(".projectbox", { scrollTrigger:{ trigger:".projectbox", start: "top 70%" }, stagger: 0.1, y: 100, opacity: 0, duration: 1, ease: "back"});
-gsap.from("#c2", {scrollTrigger: { trigger:"#c2", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: 1, ease: "back"});
-gsap.from(".projectbox2", { scrollTrigger:{ trigger:".projectbox2", start: "top 70%" }, stagger: 0.1, y: 100, opacity: 0, duration: 1, ease: "back"});
-gsap.from("#c3", {scrollTrigger: { trigger:"#c3", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: 1, ease: "back"});
-gsap.from(".projectbox3", { scrollTrigger:{ trigger:".projectbox3", start: "top 70%" }, stagger: 0.1, y: 100, opacity: 0, duration: 1, ease: "back"});
-gsap.from("#c4", {scrollTrigger: { trigger:"#c4", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: 1, ease: "back"});
-gsap.from(".projectbox4", { scrollTrigger:{ trigger:"#c4", start: "top 70%" }, delay: .5, y: 100, opacity: 0, duration: 1, ease: "back"});
+gsap.from("#c1", {scrollTrigger: { trigger:"#c1", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: .75, ease: "back"});
+gsap.from(".projectbox", { scrollTrigger:{ trigger:".projectbox", start: "top 70%" }, stagger: 0.1, y: 100, opacity: 0, duration: .75, ease: "back"});
+gsap.from("#c2", {scrollTrigger: { trigger:"#c2", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: .75, ease: "back"});
+gsap.from(".projectbox2", { scrollTrigger:{ trigger:".projectbox2", start: "top 70%" }, stagger: 0.1, y: 100, opacity: 0, duration: .75, ease: "back"});
+gsap.from("#c3", {scrollTrigger: { trigger:"#c3", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: .75, ease: "back"});
+gsap.from(".projectbox3", { scrollTrigger:{ trigger:".projectbox3", start: "top 70%" }, stagger: 0.1, y: 100, opacity: 0, duration: .75, ease: "back"});
+gsap.from("#c4", {scrollTrigger: { trigger:"#c4", start: "top 70%" }, opacity: 0, width: 0, height: 0,  duration: .75, ease: "back"});
+gsap.from(".projectbox4", { scrollTrigger:{ trigger:"#c4", start: "top 70%" }, delay: .5, y: 100, opacity: 0, duration: .75, ease: "back"});
 
 
